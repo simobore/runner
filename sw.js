d2d53450e9ef44d45be070dedccd6935e99b1ac0
simobore <borello.simone2091@gmail.com>
2026-08-31 16:49:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "runner-v7";
+const CACHE_NAME = "runner-v6-1";
 
 const FILES_TO_CACHE = [
     "./",
@@ -8,8 +8,9 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
     );
 
     self.skipWaiting();
@@ -17,13 +18,13 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
+        caches.keys().then(keys => {
+            return Promise.all(
                 keys
                     .filter(key => key !== CACHE_NAME)
                     .map(key => caches.delete(key))
-            )
-        )
+            );
+        })
     );
 
     self.clients.claim();
@@ -40,12 +41,15 @@ self.addEventListener("fetch", event => {
                 ) {
                     const copy = response.clone();
 
-                    caches.open(CACHE_NAME)
-                        .then(cache => cache.put(event.request, copy));
+                    caches.open(CACHE_NAME).then(cache => {
+                        cache.put(event.request, copy);
+                    });
                 }
 
                 return response;
             })
-            .catch(() => caches.match(event.request))
+            .catch(() => {
+                return caches.match(event.request);
+            })
     );
 });
